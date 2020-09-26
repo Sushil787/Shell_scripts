9a1992dd9581bff27dc6d9bc805bf14d24e265fb
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-09-26 18:14:20 +0545

    Create shellScriptForEvenOdd.sh

---
+++ shellScriptForEvenOdd.sh
@@ -0,0 +1,13 @@
+
+echo "input the no you like"
+read no
+    if [ $((no%2)) -eq 0 ]
+    then
+        echo " the no is even"
+     
+    
+    else {
+        echo "the no is odd"
+        
+    }
+    fi
